906f5f05feb5b3efc3999b0e3b7962ad3a27fdc1
JOHN CARLOS ARRIETA ARRIETA <johncarlosaarrietaarrieta@gmail.com>
2021-11-07 23:32:49 -0500

    Ejemplo de Login con OAuth2 con GitHub y Spring

---
+++ src/main/java/co/edu/usa/misiontic/ejemplooauth2/g5/EjemploOath2g5Application.java
@@ -0,0 +1,13 @@
+package co.edu.usa.misiontic.ejemplooauth2.g5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploOath2g5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploOath2g5Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/misiontic/ejemplooauth2/g5/EjemploOath2g5ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.misiontic.ejemplooauth2.g5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploOath2g5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
